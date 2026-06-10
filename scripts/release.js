0328122dc44fe87d0bdf3d2f7f5586534cf9f17c
Sirapat <sirapat@INDMAC03087.local>
2026-06-10 13:25:14 +0700

    add packages

--- scripts/release.js
+++ scripts/release.js
@@ -1,7 +1,9 @@
-const fs = require("fs-extra");
+import fs from "fs-extra";
 
 const consumers = {
   A: ["@wl/feature-login", "@wl/feature-dashboard"],
   B: ["@wl/feature-settings"],
   C: ["@wl/feature-login", "@wl/feature-settings"],
 };
+
+console.log(consumers);
